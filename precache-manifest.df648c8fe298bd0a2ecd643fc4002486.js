04a6767f4138e76fc008913fa3ffef81ee1fdbaf
YuraBobeshko <gorobech55@gmail.com>
2020-01-08 23:32:09 +0200

    Updates

---
+++ precache-manifest.df648c8fe298bd0a2ecd643fc4002486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92856080966da12c695b75197020299b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cff01bd5b4a458408faa",
+    "url": "/static/css/main.16a55e42.chunk.css"
+  },
+  {
+    "revision": "036054d30a056df6115f",
+    "url": "/static/js/2.789a6a10.chunk.js"
+  },
+  {
+    "revision": "b4439fda37f84e74fdbc66e669b783a5",
+    "url": "/static/js/2.789a6a10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cff01bd5b4a458408faa",
+    "url": "/static/js/main.9eb8dddb.chunk.js"
+  },
+  {
+    "revision": "696942293a92ac3a2bb6",
+    "url": "/static/js/runtime-main.889ad4b6.js"
+  }
+]);
